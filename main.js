937e8852cd96c44fbcc63d6cb16b004026bf836e
FilippoBid <filippoperrone1997@gmail.com>
2022-10-26 17:17:31 +0200

    fine dei giochi

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+const nome = prompt("inserisci il tuo nome");
+const cognome =  prompt("inserisci il tuo cognome");
+const colore =  prompt("inserisci il tuo colore preferito");
 
-
-
-const nome = prompt ("inserisci il nome")
-const cognome = 
-const colore = 
+const h2NomeCognomeColore = document.getElementById('maintext')
+const passwordGenerata = document.getElementById('password')
+password.innerHTML = `la tua password:${nome}${cognome}${colore}21`
+h2NomeCognomeColore.innerHTML = `ti chiami ${nome} ${cognome} ,il tuo colore preferito e il ${colore}`
